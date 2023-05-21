1bb1c109dea4ef02af10bf2013a6170446e878b8
Lucas Trindade <trindadebra@gmail.com>
2023-05-21 18:11:53 -0300

    get user

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import {Router, Request, Response} from "express";
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { DetailuserController } from "./controllers/user/DetailUserController";
 
 const router = Router();
 
@@ -12,5 +14,7 @@ router.get("/test", (request: Request, response: Response) => {
 router.post('/user', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
+
+router.get('/me', isAuthenticated, new DetailuserController().handle) //middleware add
 
 export {router}
